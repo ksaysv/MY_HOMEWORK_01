3e3a8ada9de0e5c3ac8bbae3cbfffcb187a71081
ksaysv <ksaysv@mail.ru>
2024-10-04 11:45:51 +0300

    sep/end

---
+++ Push.py
@@ -0,0 +1,38 @@
+print('Python', end='+')  # print('C++')
+print('C#', end='=')  # print('C')
+print('awesome')
+print()
+print('Python', end='\n\n\n')
+print()
+print('a', '\n', 'b', '\n', 'c', sep='*', end='#')
+print()
+print('a', 'b', 'c', sep='*', end='finish')
+print()
+arg1 = 'Hello'
+sep1 = '_-_'
+end2 = '+++'
+print(arg1, 'everyone', sep=sep1, end='! ')
+print('How', 'are', 'you', 'in', '2024?', sep=' ', end=end2)
+print()
+print('a', 'b', 'c', sep='', end='')
+print('d', 'e', 'f', sep='', end='')
+print()
+delimiter = '+'
+print('a', 'b', sep=delimiter)
+print()
+print('Mercury', 'Venus', sep='*', end='!')
+print('Mars', 'Jupiter', sep='**', end='?')
+print()
+sep1 = '#'
+end2 = '?'
+print('YES', 'NO', sep=sep1, end='+')
+print('MAYBE', 'NEVER', sep='*', end=end2)
+print()
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
