d7a34452f9314becadafeac07ba2bf92eacef666
ksaysv <ksaysv@mail.ru>
2024-12-08 20:01:53 +0300

    ок

--- module_4_2.py
+++ module_4_2.py
@@ -2,6 +2,6 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function') # ничего не отображает интерпретатор
 
-inner_function() # ошибка NameError: name 'inner_function' is not defined
+inner_function() # ошибка NameError: name 'inner_function' is not definedв
 
 
