b69371831614013a15df6fce0f7d30280bf9d68c
ksaysv <ksaysv@mail.ru>
2024-12-08 19:47:32 +0300

    ок

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function') # ничего не отображает интерпретатор
+
+inner_function() # ошибка NameError: name 'inner_function' is not defined
+
+
